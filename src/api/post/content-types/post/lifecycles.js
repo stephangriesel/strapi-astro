9f81f99b92bdd092780836d0c04b01018ab0e875
Stephan Griesel <sgriesel@gmail.com>
2023-11-17 07:29:59 +0100

    fix

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -1,19 +1,22 @@
 const readingTime = require("reading-time");
 
 module.exports = {
-  async beforeCreate(event) {
-    console.log("########## BEFORE CREATE ##########");
-    if (event.params.data.content) {
-      event.params.data.readingTime =
-      readingTime(event.params.data.content)?.text || null;
+  beforeCreate(event) {
+    const { data, where, select, populate } = event.params;
+
+    if (event.params.data.content && event.params.data.content?.length > 0) {
+      // @ts-ignore
+      event.params.data.readingTime = readingTime(event.params.data.content)?.text || null;
     }
   },
 
-  async beforeUpdate(event) {
-    console.log("########## BEFORE UPDATE ##########");
-    if (event.params.data.content) {
-      event.params.data.readingTime =
-      readingTime(event.params.data.content)?.text || null;
+  beforeUpdate(event) {
+    const { data, where, select, populate } = event.params;
+
+    if (event.params.data.content && event.params.data.content?.length > 0) {
+      // @ts-ignore
+      event.params.data.readingTime = readingTime(event.params.data.content)?.text || null;
     }
   },
+
 };
